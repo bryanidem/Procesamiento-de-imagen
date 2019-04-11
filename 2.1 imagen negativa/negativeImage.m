99d1eda7c8f4e345e47a077970325909184657b7
Bryan Medina <bryan.anidem@gmail.com>
2019-04-10 21:23:15 -0500

    Update negativeImage.m
    Implementación en MATLAB

--- 2.1 imagen negativa/negativeImage.m
+++ 2.1 imagen negativa/negativeImage.m
@@ -1,5 +1,5 @@
-% obtención del complemento de una imagen.
-
+% obtenciÃ³n del complemento de una imagen.
+ 
 %cargar la imagen a la variable f (de tipo uint8 con 3 canales)
 f = imread('chest.jpg');
 
