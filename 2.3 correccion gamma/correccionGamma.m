188f3c678c013ec234f0948b1b8db6986bbb6d04
Bryan Medina <bryan.anidem@gmail.com>
2019-05-15 17:53:28 -0500

    Update correccionGamma.m

--- 2.3 correccion gamma/correccionGamma.m
+++ 2.3 correccion gamma/correccionGamma.m
@@ -6,18 +6,19 @@
 f = imread('sigure.jpg');
 f = rgb2gray(f);
 
-%constante segun para poner en el mismo rango de la imagen pero no me
-%funciono del todo, y la mayoria utiliza c = 1
-c = 255/log(1 + 255);
-
 gamma = 0.2;
 gamma1 = 5;
+
+%constante  para poner en el mismo rango de la imagen
+
+c = 255/(255^gamma);
+c1 = 255/(255^gamma1);
+
 % primero se realiza un escalamiento de la intensidad de la imagen de 0 a 1
 % donde estos valores despues son mapeados a los 256 valores de intensidad
-g = im2uint8(mat2gray(1 * double(f).^gamma));
-g1 = im2uint8(mat2gray(1 * double(f).^gamma1));
+g = im2uint8(mat2gray(c * double(f).^gamma));
+g1 = im2uint8(mat2gray(c1 * double(f).^gamma1));
 
-% una mejor version, con funciones esta en python
 
 figure;
 subplot(1, 3, 1);
@@ -31,4 +32,3 @@ title('gamma = 0.2');
 subplot(1, 3, 3)
 imshow(g1);
 title('gamma = 5');
-
